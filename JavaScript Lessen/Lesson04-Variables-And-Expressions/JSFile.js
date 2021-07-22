da4ff86d7133ba9afefbbf2d22175be983c06949
MartijnReede <martijn.reede@gmail.com>
2021-07-22 23:01:44 +0200

    Les 5 geupload.

---
+++ JavaScript Lessen/Lesson04-Variables-And-Expressions/JSFile.js
@@ -0,0 +1,18 @@
+// Javascript houdt geen rekening met datatype. Je hoeft dus bijvoorbeeld
+// niet aan te geven dat de variabele een Integer of String is. Alles is
+// een "var". Onderstaand het bewijs:
+
+var username = 5;
+username = "vijf";
+
+//Een variabele die nog geen waarde heeft krijgt by default de waarde "undefined".
+var undefined;
+
+
+//Een expressie is een stukje code dat een bepaalde waarde als resultaat heeft.
+//Bijvoorbeeld:
+
+var x = 5;
+var x = getNumber();
+
+// Zowel 5 als getNumber() zijn expressies. 
