85371180ccd3280110c8529cd0073f9d78d0b26a
MartijnReede <martijn.reede@gmail.com>
2021-07-22 22:09:37 +0200

    Upload les 3 JavaScript

---
+++ JavaScript Lessen/Lesson03-Basics,Input,Output,Comments/JSFile2.js
@@ -0,0 +1,11 @@
+//Variabelen kunnen worden aangemaakt d.m.v. het var-keyword.
+
+//Met prompt kunnen we een pop-up genereren met een invoerbalk.
+//Met alert kunnen we een pop-up genereren met een bepaalde waarde,
+//bijvoorbeeld de variabele username.
+//Met console.log() kunenn we ook iets printen, maar dan zonder
+//pop-up.  
+
+var username = prompt();
+  alert("Hello " + username + "!");
+console.log(username);
