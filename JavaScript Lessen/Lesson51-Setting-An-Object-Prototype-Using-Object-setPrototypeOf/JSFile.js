ee5f2b479d2e9d2106c5946c6513aa26af1ddb97
MartijnReede <martijn.reede@gmail.com>
2021-07-31 16:21:24 +0200

    Upload JS lessen

--- JavaScript Lessen/Lesson51-Setting-An-Object-Prototype-Using-Object-setPrototypeOf/JSFile.js
+++ JavaScript Lessen/Lesson51-Setting-An-Object-Prototype-Using-Object-setPrototypeOf/JSFile.js
@@ -1,7 +1,36 @@
+/*
+
+  We hunnen het prototypeobject handmatig instellen. Onderstaand een voorbeeld:
+
+*/
 
 
 {
+  let userType = {
+    active: true,
+    displayUser: function(){
+      console.log(this);
+    }
+  }
 
+  let teacher = {
+    teaching: ["Math", "Science"]
+  }
 
+  let student = {
+    learning: ["nothing"]
+  }
 
+  Object.setPrototypeOf(teacher, userType);
+  Object.setPrototypeOf(student, userType);
+
+  console.log(teacher.active);
+  teacher.displayUser();
+
+  console.log(student.active);
+  student.displayUser();
+
+  //Teacher is nu eigenlijk inherited van userType.
+  //We zouden nu bijvoorbeeld ook een Student kunnen maken die ook van userType
+  //inherited is.
 }
