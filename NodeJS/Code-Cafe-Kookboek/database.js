f8225e5c97ded426351ef78fd7679d40688fa2a7
MartijnReede <martijn.reede@gmail.com>
2021-08-02 22:01:36 +0200

    Upload NodeJS Project

---
+++ NodeJS/Code-Cafe-Kookboek/database.js
@@ -0,0 +1,4 @@
+//Verbose betekend dat we wat meer berichten krijgen over of iets goed of fout gaat.
+const sqlite3 = require('sqlite3').verbose();
+
+
--- NodeJS/Code-Cafe-Kookboek/index.js
+++ NodeJS/Code-Cafe-Kookboek/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
-const PORT = 6000;
+const PORT = 8080;
 const app = express();
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({status: "server running"});
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}.`);
